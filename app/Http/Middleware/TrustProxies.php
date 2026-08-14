0e42f0c2143488dddbdba40c38f9fc7f78e927b3
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2026-08-14 17:06:18 +0800

    fix bug: proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,8 +12,8 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies = '*';
-
+    protected $proxies = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16', '127.0.0.1/8'];
+    
     /**
      * The headers that should be used to detect proxies.
      *
